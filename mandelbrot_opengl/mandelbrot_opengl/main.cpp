8f9d18f9672430475f2edb8d519f4d632899315b
Tom Mason <masonworldt@gmail.com>
2021-09-26 22:29:07 +0100

    added GLEW

--- mandelbrot_opengl/mandelbrot_opengl/main.cpp
+++ mandelbrot_opengl/mandelbrot_opengl/main.cpp
@@ -1,24 +1,34 @@
 
+#include <iostream>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main()
 {
     GLFWwindow* window;
-
+    
     if (!glfwInit())
         return -1;
-
+    
     window = glfwCreateWindow(640, 480, "Hello Mandelbrot", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
-
+    
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK)
+        std::cout << "glewInit error\n";
+    
     while (!glfwWindowShouldClose(window))
     {
-
+        glClear(GL_COLOR_BUFFER_BIT);
+    
+        glfwSwapBuffers(window);
+    
+        glfwPollEvents();
     }
 }
