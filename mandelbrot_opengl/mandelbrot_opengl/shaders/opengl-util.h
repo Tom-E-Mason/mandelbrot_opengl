72a449f3675e7c1c97447b4e06bc4413b76dcee7
Tom Mason <masonworldt@gmail.com>
2021-09-26 22:44:48 +0100

    added opengl-util

---
+++ mandelbrot_opengl/mandelbrot_opengl/shaders/opengl-util.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include <string>
+#include <fstream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class shader
+{
+public:
+    shader(std::string path)
+    {
+
+    }
+
+private:
+
+};
