83f74daf67be3b69bcda15dada31fda91d85a421
Tom Mason <masonworldt@gmail.com>
2021-09-27 17:01:27 +0100

    pre branch commit

--- mandelbrot_opengl/mandelbrot_opengl/main.cpp
+++ mandelbrot_opengl/mandelbrot_opengl/main.cpp
@@ -1,49 +1,20 @@
 
 #include <iostream>
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include "opengl-util.h"
 
-static auto compile_shader(const std::string& source, unsigned int type)
-{
-    auto id = glCreateShader(type);
-    const char* src = source.c_str();
-
-    glShaderSource(id, 1, &src, NULL);
-    glCompileShader(id);
-
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE)
-    {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-
-        std::string message;
-        message.reserve(length);
-
-        glGetShaderInfoLog(id, length, &length, message.data());
-
-        std::cout << message << '\n';
-    }
-
-    return id;
-}
-
-static auto create_shader(const std::string& vertex_shader, const std::string& fragment_shader)
+static auto create_program(const shader& vertex_shader, const shader& fragment_shader)
 {
     auto program = glCreateProgram();
-    auto vs = compile_shader(vertex_shader, GL_VERTEX_SHADER);
-    auto fs = compile_shader(fragment_shader, GL_FRAGMENT_SHADER);
 
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
+    glAttachShader(program, vertex_shader.id());
+    glAttachShader(program, fragment_shader.id());
 
     glLinkProgram(program);
     glValidateProgram(program);
 
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+    glDeleteShader(vertex_shader.id());
+    glDeleteShader(fragment_shader.id());
 
     return program;
 }
@@ -55,7 +26,9 @@ int main()
     if (!glfwInit())
         return -1;
     
-    window = glfwCreateWindow(640, 480, "Hello Mandelbrot", NULL, NULL);
+    int screen_width = 640;
+    int screen_height = 480;
+    window = glfwCreateWindow(screen_width, screen_height, "Hello Mandelbrot", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -67,50 +40,59 @@ int main()
     if (glewInit() != GLEW_OK)
         std::cout << "glewInit error\n";
 
-    float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+    float positions[] =
+    {
+        -1.0f, -1.0f,
+         1.0f, -1.0f,
+         1.0f,  1.0f,
+        -1.0f,  1.0f,
+    };
+
+    uint32_t indices[] =
+    {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    unsigned int index_buffer;
+    glGenBuffers(1, &index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 2 * sizeof(float), indices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-    std::string vertex_shader =
-        "\n"
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = position;\n"
-        "}\n";
+    auto vertex_shader = shader("shaders/v.glsl", GL_VERTEX_SHADER);
+    auto fragment_shader = shader("shaders/fragment_shader.glsl", GL_FRAGMENT_SHADER);
 
-    std::string fragment_shader =
-        "\n"
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) out vec4 colour;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "    colour = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n";
+    const auto program = create_program(vertex_shader, fragment_shader);
+    glUseProgram(program);
 
-    auto shader = create_shader(vertex_shader, fragment_shader);
-    glUseProgram(shader);
+    int u_offset = glGetUniformLocation(program, "u_offset");
+    float offsetx = 0.0f, offsety = 0.0f;
+    glUniform2f(u_offset, offsetx, offsety);
+
+    int u_scale = glGetUniformLocation(program, "u_scale");
+    float scalex = (float)screen_width * 2, scaley = (float)screen_height;
+    glUniform2f(u_scale, scalex, scaley);
+
+    int u_iterations = glGetUniformLocation(program, "u_iterations");
+    int iterations = 4096;
+    glUniform1d(u_iterations, iterations);
+
+    int u_screen = glGetUniformLocation(program, "u_iterations");
+    glUniform2f(u_screen, (float)screen_width, (float)screen_height);
     
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
     
         glfwSwapBuffers(window);
     
---
+++ mandelbrot_opengl/mandelbrot_opengl/opengl-util.h
@@ -0,0 +1,53 @@
+
+#pragma once
+
+#include <string>
+#include <fstream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class shader
+{
+public:
+    shader(std::string path, uint32_t type)
+    {
+        auto ifs = std::fstream(path);
+
+        std::string buf;
+        while (std::getline(ifs, buf))
+        {
+            m_source += buf;
+            m_source += '\n';
+        }
+
+        m_id = glCreateShader(type);
+        const char* src = m_source.c_str();
+
+        glShaderSource(m_id, 1, &src, NULL);
+        glCompileShader(m_id);
+
+        int result;
+        glGetShaderiv(m_id, GL_COMPILE_STATUS, &result);
+        if (result == GL_FALSE)
+        {
+            int length;
+            glGetShaderiv(m_id, GL_INFO_LOG_LENGTH, &length);
+
+            std::string message;
+            message.reserve(length);
+
+            glGetShaderInfoLog(m_id, length, &length, message.data());
+
+            std::cout << message << '\n';
+        }
+    }
+
+    const char* c_str() const { return m_source.c_str(); }
+    auto string() const { return m_source; }
+    auto id() const { return m_id; }
+
+private:
+    uint32_t m_id;
+    std::string m_source;
+};
